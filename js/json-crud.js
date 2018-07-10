206a47c9bda8b9fc08dd5130e86453eac405f527
Iliyas <iliyas.zholdasbayev@gmail.com>
2018-07-10 14:51:54 +0200

    feat(pdf): start implementing pdf viewer feature

--- js/json-crud.js
+++ js/json-crud.js
@@ -46,17 +46,30 @@ function saveToFile(data) {
     });
 }
 
+$('#editBattleModal').on('show.bs.modal', function(event) {
+    // console.log("sdsd" + data);
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var data = button.data('whatever') // Extract info from data-* attributes
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    console.log("sdsd");
+
+    // var modal = $(this)
+    // modal.find('.modal-title').text('The ' + data.name + ' battle')
+    // modal.find('.modal-body input').val(recipient)
+})
+
 $(document).ready(function() {
     d3.json("/data/all_battles_new.json", function(error, data) {
         if (error) throw error;
 
         for (var i in data) {
             var listItem = "<a id=" + data[i].id + " class='list-group-item flex-column align-items-start'>" +
-                "<div class='d-flex w-100 justify-content-between'>" +
-                "<button data-target='#col" + data[i].id + "' data-toggle='collapse' role='button' aria-expanded='false' aria-controls='col" + data[i].id + "'>" + data[i].properties.name + "</button>" +
+                "<div class='d-flex w-100 justify-content-between'>" + "<div>" + data[i].properties.name +
+                "<button class='btn collapse-btn' data-target='#col" + data[i].id + "' data-toggle='collapse' role='button' aria-expanded='false' aria-controls='col" + data[i].id + "'>" + "<i class='fas fa-caret-down'></i>" + "</button></div>" +
                 "<small>" +
-                "<button><i class='fas fa-edit'></i></button>" +
-                "<button onclick=deleteItem($(this).closest('a').attr('id'))><i class='fas fa-times-circle'></i></button>" +
+                "<button class='btn icons-btn' type='button' data-toggle='modal' data-target='#editBattleModal' data-whatever='" + data[i] + "'><i class='fas fa-edit'></i></button>" +
+                "<button class='btn icons-btn' onclick=deleteItem($(this).closest('a').attr('id'))><i class='fas fa-times-circle'></i></button>" +
                 "</small></div></a>"
 
             var collapse = "<div class='collapse' id='col" + data[i].id + "'>" +
@@ -68,7 +81,13 @@ $(document).ready(function() {
             $('#battles-list .list-group').append(listItem + collapse);
         }
     });
+    $('#editBattleModal').on('hidden.bs.modal', function(e) {
+        // do something...
+        console.log("asdasdasd");
+
+    })
 });
+
 
 function deleteItem(itemId) {
     var confirmRes = confirm("Sure?");
